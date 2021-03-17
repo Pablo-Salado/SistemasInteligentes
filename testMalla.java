bf2bdb5876e952835ada8dc5aa45310252487892
Winters <andreslp0001@gmail.com>
2021-03-17 15:42:52 +0100

    Añadidos los ficheros test y Malla

---
+++ testMalla.java
@@ -0,0 +1,8 @@
+public class testMalla {
+
+    public static void main(String [] args) {
+        Malla m = new Malla(0, 3, 3, 2);
+        m.ver();
+        System.out.println(m.getInicio());
+    }
+}
