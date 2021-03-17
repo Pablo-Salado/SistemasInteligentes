bf17f91ac35477ff0143029578e5e333feb12032
PabloSalado <0619980048@uma.es>
2021-03-17 16:04:18 +0100

    v1

--- malla.java
+++ malla.java
@@ -2,12 +2,39 @@ package Malla.SistemasInteligentes;
 
 import java.util.Random;
 
-public class malla {
+public class Malla {
     final int gFilas;
     final int gColumnas;
     char[][] gMatrix;
 
-    public malla(long seed, int filas, int columnas, int obstaculos) {
+    public String getInicio() {
+        return inicio.toString();
+    }
+
+    public String getEnd() {
+        return end.toString();
+    }
+
+    Tuple inicio;
+    Tuple end;
+
+    private class Tuple{
+        final int fila;
+        final int columna;
+        public Tuple(int A, int B){
+            fila=A;
+            columna = B;
+        }
+
+        public String toString(){
+            String res = "La coordenada es: (";
+            res+= fila + ", "+columna+")";
+            return  res;
+        }
+
+    }
+
+    public Malla(long seed, int filas, int columnas, int obstaculos) {
         Random S = new Random();
         S.setSeed(seed);
 
@@ -33,22 +60,24 @@ public class malla {
     }
 
     public void generarInicioyFinal (Random seed) {
-        int rndFila = seed.nextInt(gFilas);
-        int rndColumna = seed.nextInt(gColumnas);
-        boolean ini = false,check = false;
-        while(!check) {
-            if (!ocupada(rndFila, rndColumna) && ini) {
-                gMatrix[rndFila][rndColumna] = 'F';
-                check = true;
-            }else if(!ocupada(rndFila, rndColumna) && !ini){
-                gMatrix[rndFila][rndColumna] = 'I';
-                ini = true;
-            }else{
-                rndFila = seed.nextInt(gFilas);
-                rndColumna = seed.nextInt(gColumnas);
+            int rndFila = seed.nextInt(gFilas);
+            int rndColumna = seed.nextInt(gColumnas);
+            boolean ini = false,check = false;
+            while(!check) {
+                if (!ocupada(rndFila, rndColumna) && ini) {
+                    gMatrix[rndFila][rndColumna] = 'F';
+                    check = true;
+                    end = new Tuple(rndFila,rndColumna);
+                }else if(!ocupada(rndFila, rndColumna) && !ini){
+                    gMatrix[rndFila][rndColumna] = 'I';
+                    ini = true;
+                    inicio = new Tuple(rndFila,rndColumna);
+                }else{
+                    rndFila = seed.nextInt(gFilas);
+                    rndColumna = seed.nextInt(gColumnas);
+                }
             }
         }
-    }
 
 
     private boolean ocupada (int fila, int columna) {
@@ -76,5 +105,3 @@ public class malla {
         }
     }
 }
-
-
