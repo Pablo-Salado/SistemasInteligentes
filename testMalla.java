3ac9c0efad1762a6acf31f47f74f5ba4f9f500d6
Galoan012 <0619971215@uma.es>
2021-03-17 16:35:14 +0100

    Añadidos archivos en blanco

--- testMalla.java
+++ testMalla.java
@@ -1,8 +1,4 @@
-public class testMalla {
+package SistemasInteligentes;
 
-    public static void main(String [] args) {
-        Malla m = new Malla(0, 3, 3, 2);
-        m.ver();
-        System.out.println(m.getInicio());
-    }
+public class testMalla {
 }
