27bf09b7030c43d1a80572b3ae3814da5452e079
PabloSalado <0619980048@uma.es>
2021-03-18 10:36:38 +0100

    Modificado EstadoMalla

--- EstadoMalla.java
+++ EstadoMalla.java
@@ -5,4 +5,23 @@ import java.util.List;
 
 public class EstadoMalla implements Estado {
 
+    @Override
+    public List<? extends Estado> calculaSucesores() {
+        return null;
+    }
+
+    @Override
+    public int coste(Estado e2) {
+        return 0;
+    }
+
+    @Override
+    public int h(Estado objetivo) {
+        return 0;
+    }
+
+    @Override
+    public void ver() {
+
+    }
 }
