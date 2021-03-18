d3f0046522796150abdb8cb555e348ca8c7a5ef9
PabloSalado <0619980048@uma.es>
2021-03-18 10:06:34 +0100

    Añadida clase estadomalla

--- Estado.java
+++ Estado.java
@@ -7,12 +7,14 @@ public interface Estado {
     /**
      * @return lista con los estados sucesores inmediatos del estado actual en el espacio de estados
      */
+
     public List<? extends Estado> calculaSucesores();
 
     /**
      * @param e2
      * @return coste del arco que conecta el estado actual con el estado e2
      */
+
     public int coste(Estado e2);
 
     /**
