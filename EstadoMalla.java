d394651e8be399b3f12d436c18d419f5981f50b1
adriguerre <ADGA@uma.es>
2021-03-18 10:33:45 +0100

    Modificado CalculaSucesores();

--- EstadoMalla.java
+++ EstadoMalla.java
@@ -15,9 +15,25 @@ public class EstadoMalla implements Estado{
     @Override
     public List<? extends Estado> calculaSucesores() {
         List<Estado> estado = new ArrayList<>();
+        int cont = 0;
 
-        return null;
+        while(cont < 4){
+                if(OutOfMatrix(matriz.posI)){
+                        cont++;
+                }else{
+
+                }
+
+        }
+
+
+        return estado;
     }
+
+    private boolean OutOfMatrix(Tuple posI) {
+        return posI.fila >= 0 && posI.fila < matriz.gFilas && posI.columna >= 0 && posI.columna < matriz.gColumnas;
+    }
+
 
     @Override
     public int coste(Estado e2) {
