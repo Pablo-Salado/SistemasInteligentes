e73df141ec554d1720d871243b8f45474cb06345
PabloSalado <0619980048@uma.es>
2021-03-18 14:15:18 +0100

    Primera version de Estadomalla

--- EstadoMalla.java
+++ EstadoMalla.java
@@ -9,29 +9,32 @@ public class EstadoMalla implements Estado{
 
     public EstadoMalla(Malla mat){
         matriz = mat;
-
     }
 
     @Override
     public List<? extends Estado> calculaSucesores() {
         List<Estado> estado = new ArrayList<>();
         int cont = 0;
+        Tuple sucesorActual = matriz.getPosI();
 
         while(cont < 4){
-            if(OutOfMatrix(matriz.getPosI())){
+            //asignar a sucesorActual el primer posible sucesor
+            if(InMalla(sucesorActual) && esHueco(sucesorActual)){ //si está en la malla y es un hueco
+                estado.add(sucesorActual); //añadirlo a la lista de estados
                 cont++;
             }else{
-
+                //o esta fuera de la matriz o es un obstaculo por tanto no se considera sucesor
             }
-
         }
-
-
         return estado;
     }
 
-    private boolean OutOfMatrix(Tuple posI) {
+    private boolean InMalla(Tuple posI) {
         return posI.fila >= 0 && posI.fila < matriz.getFilas() && posI.columna >= 0 && posI.columna < matriz.getColumnas();
+    }
+
+    private boolean esHueco (Tuple sucesor) {
+        return false;
     }
 
 
