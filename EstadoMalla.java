fbe987779ae814392d28b544173fe2a0500856bb
PabloSalado <0619980048@uma.es>
2021-03-18 13:52:08 +0100

    Creada clase tuple

--- EstadoMalla.java
+++ EstadoMalla.java
@@ -3,12 +3,37 @@ package SistemasInteligentes;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EstadoMalla implements Estado {
+public class EstadoMalla implements Estado{
+
+    static Malla matriz;
+
+    public EstadoMalla(Malla mat){
+        matriz = mat;
+
+    }
 
     @Override
     public List<? extends Estado> calculaSucesores() {
-        return null;
+        List<Estado> estado = new ArrayList<>();
+        int cont = 0;
+
+        while(cont < 4){
+            if(OutOfMatrix(matriz.posI)){
+                cont++;
+            }else{
+
+            }
+
+        }
+
+
+        return estado;
     }
+
+    private boolean OutOfMatrix(Tuple posI) {
+        return posI.fila >= 0 && posI.fila < matriz.gFilas && posI.columna >= 0 && posI.columna < matriz.gColumnas;
+    }
+
 
     @Override
     public int coste(Estado e2) {
@@ -24,4 +49,14 @@ public class EstadoMalla implements Estado {
     public void ver() {
 
     }
+
+    public boolean equals(){
+        return false;
+    }
+
+    public int hashCode(){
+        return 0;
+    }
+
 }
+
