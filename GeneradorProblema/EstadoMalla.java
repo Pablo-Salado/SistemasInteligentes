366b9090159a588e3a522007e75c759fc4b4b8db
Galoan012 <0619971215@uma.es>
2021-03-24 17:05:50 +0100

    añadido método calculaSucesores

--- GeneradorProblema/EstadoMalla.java
+++ GeneradorProblema/EstadoMalla.java
@@ -20,7 +20,36 @@ public class EstadoMalla implements Estado{
 
     @Override
     public List<? extends Estado> calculaSucesores() {
+        List<EstadoMalla> estados = new ArrayList<>();
 
+        Tuple sup = actual;
+        sup.setFila(sup.fila-1);
+        Tuple inf=actual;
+        inf.setFila(inf.fila+1);
+        Tuple der=actual;
+        der.setColumna(der.columna+1);
+        Tuple izq=actual;
+        izq.setColumna(izq.columna+1);
+
+        if(esValida(sup)) {
+            EstadoMalla superior = new EstadoMalla(matriz, sup);
+            estados.add(superior);
+        }else if(esValida(inf)){
+            EstadoMalla inferior = new EstadoMalla(matriz, inf);
+            estados.add(inferior);
+        }else if(esValida(der)){
+            EstadoMalla derecha = new EstadoMalla(matriz, der);
+            estados.add(derecha);
+        }else if (esValida(izq)){
+            EstadoMalla izquierda = new EstadoMalla(matriz, izq);
+            estados.add(izquierda);
+        }
+
+        return estados;
+    }
+
+    private boolean esValida(Tuple tuple) {
+        return ((tuple.fila>=0 && tuple.fila<=matriz.gFilas) && (tuple.columna>=0 && tuple.columna<=matriz.gColumnas)) && (matriz.gMatrix[tuple.fila][tuple.columna] =='-');
     }
 
     @Override
