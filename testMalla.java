8c9706bf999d8efef4db7562b377799f5cc22bdb
Galoan012 <0619971215@uma.es>
2021-03-24 16:47:05 +0100

    IDK

--- testMalla.java
+++ testMalla.java
@@ -6,8 +6,7 @@ import SistemasInteligentes.GeneradorProblema.Malla;
 public class testMalla {
 
     public static void main(String [] args) {
-        Malla campo = new Malla(1, 3, 5, 4);
-
+        Malla campo = new Malla(1, 3, 3, 4);
         campo.ver();
         System.out.println("La posición inicial es: " + campo.getPosI());
         System.out.println("La posición final es: " + campo.getPosF());
