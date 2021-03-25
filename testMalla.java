2d678d1f9ae125d6dd7d70c9a5a68c7153a14ffb
Galoan012 <0619971215@uma.es>
2021-03-25 11:17:45 +0100

    Test calculaEstados()

--- testMalla.java
+++ testMalla.java
@@ -1,14 +1,20 @@
 package SistemasInteligentes;
 
+import SistemasInteligentes.GeneradorProblema.Estado;
 import SistemasInteligentes.GeneradorProblema.EstadoMalla;
 import SistemasInteligentes.GeneradorProblema.Malla;
 
 public class testMalla {
 
     public static void main(String [] args) {
-        Malla campo = new Malla(1, 3, 3, 4);
+        Malla campo = new Malla(0, 5, 3, 4);
         campo.ver();
         System.out.println("La posición inicial es: " + campo.getPosI());
         System.out.println("La posición final es: " + campo.getPosF());
+
+        EstadoMalla test = new EstadoMalla(campo,campo.getPosI());
+        for(Estado x:test.calculaSucesores()){
+            System.out.println(x.toString());
+        }
     }
 }
