cba2e9688b224ed6462fc7b98ca31cf8912549da
PabloSalado <0619980048@uma.es>
2021-04-11 13:23:44 +0200

    creado PriorityQueueAbiertos

---
+++ PriorityQueueAbiertos.java
@@ -0,0 +1,33 @@
+package SistemasInteligentes;
+
+public class PriorityQueueAbiertos extends Abiertos{
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void offer(int f, Estado estado) {
+
+    }
+
+    @Override
+    public Estado poll() {
+        return null;
+    }
+
+    @Override
+    public void remove(Estado estado) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void ver() {
+
+    }
+}
