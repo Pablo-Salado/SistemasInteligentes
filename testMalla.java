4779c33708f07d543a03e167d1546bcfaf14f897
PabloSalado <0619980048@uma.es>
2021-04-11 13:40:55 +0200

    actualizado testMalla *Ver que hace el lista.offer

--- testMalla.java
+++ testMalla.java
@@ -1,5 +1,7 @@
 package SistemasInteligentes;
 
+import java.util.Iterator;
+
 public class testMalla {
 
     public static void main(String [] args) {
@@ -21,5 +23,55 @@ public class testMalla {
         System.out.println();
         System.out.println("Distancia Manhattan desde el inicio al final");
         System.out.println(estadoInicial.h(estadoFinal));
+        System.out.println();
+
+        listaAbiertos lista = new listaAbiertos();
+
+        if(lista.isEmpty()){
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("FALSE");
+        }
+
+        int h = estadoInicial.h(estadoFinal);
+        lista.offer(h,estadoFinal);
+
+        if(lista.isEmpty())
+            System.out.println("TRUE");
+        else
+            System.out.println("FALSE");
+
+        for(Estado x : estadoInicial.calculaSucesores()){
+            lista.offer(x.h(estadoFinal),x);                //ver con el depurador que hace esto
+        }
+
+        lista.ver();
+
+        Estado borrar1 = lista.poll();
+        Estado borrar2 = lista.poll();
+
+        System.out.print("Vamos a borrar el estado:");
+        borrar1.ver();
+
+        System.out.print("Vamos a borrar el estado:");
+        borrar2.ver();
+
+        lista.ver();
+        System.out.println(); System.out.println(); System.out.println();
+        System.out.println("Insertamos el estado e1");
+
+        Iterator it = lista.getList().iterator();
+
+
+
+        Estado e1 = new EstadoMalla(campo,new Tuple(0,0));
+        lista.offer(e1.h(estadoFinal),e1);
+        lista.ver();
+        System.out.println(); System.out.println(); System.out.println();
+        System.out.println("Borramos el estado e1");
+        lista.remove(e1);
+        lista.ver();
+
     }
 }
