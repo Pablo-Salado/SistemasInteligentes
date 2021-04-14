a3b9aac93d26137fb1a09c3db62f3aee7cf45389
Galoan012 <0619971215@uma.es>
2021-04-14 18:40:11 +0200

    Añadidos algunos métodos

---
+++ GeneradorProblema/PriorityQueueAbiertos.java
@@ -0,0 +1,53 @@
+package SistemasInteligentes.GeneradorProblema;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueAbiertos extends Abiertos{
+    PriorityQueue queue;
+    public PriorityQueueAbiertos(){
+        PriorityQueue q = new PriorityQueue();
+        queue = q;
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public void offer(int f, Estado estado) {
+
+    }
+
+    @Override
+    public Estado poll() {
+        Estado aux = (Estado) queue.poll();
+        queue.poll();
+        return aux;
+    }
+
+    @Override
+    public void remove(Estado estado) {
+        Iterator it = queue.iterator();
+        int i = 0;
+        while (it.hasNext() && !(it.equals(estado))){
+            i++;
+            it.next();
+        }
+        if(i>=queue.size()){
+            throw new RuntimeException("Estado no encontrado");
+        }else queue.remove(it);
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void ver() {
+
+    }
+}
