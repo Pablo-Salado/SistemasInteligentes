f2ab99d3841b5b38f74db798f3d234899a22fa91
PabloSalado <0619980048@uma.es>
2021-04-27 18:06:44 +0200

    modificado LinkedLista

--- LinkedLista.java
+++ LinkedLista.java
@@ -9,25 +9,41 @@ public class LinkedLista<E> extends Arbol{
     //Introduce el nodo n en el arbol
     @Override
     public void put(Nodo nodo) {
-
-        if(lista.isEmpty()){ //Ponemos el padre
-            lista.add(nodo);
-            nodo.setPadre(nodo);
-        }else{ //Tenemos que insertar el elemento
-            lista.add(nodo);
-        }
+        lista.add(nodo);
     }
 
     //True si e arbol contiene el estado indicado, false en otro caso
     @Override
     public boolean containsKey(Estado estado) {
-        return false;
+        int i = 0;
+
+        boolean res = false;
+        while(i < lista.size() && !res){
+
+            if(lista.get(i).e.equals(estado))
+                res = true;
+            else
+                i++;
+        }
+        return res;
     }
 
     //Devolvera el nodo asociado al estado e en el arbol
     @Override
     public Nodo get(Estado estado) {
-        return null;
+        int i = 0;
+        boolean res = false;
+        Nodo resNodo = null;
+        while(i < lista.size() && !res) {
+
+            if (lista.get(i).e.equals(estado)) {
+                resNodo = lista.get(i);
+                res = true;
+            } else {
+                i++;
+            }
+        }
+        return resNodo;
     }
 
     //Imprime el contenido de la lista
