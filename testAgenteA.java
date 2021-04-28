1aa3bcda00631b7a2eb3c86d1a7677d2c0279eb0
PabloSalado <0619980048@uma.es>
2021-04-28 18:11:49 +0200

    pruebas AgenteA

--- testAgenteA.java
+++ testAgenteA.java
@@ -2,20 +2,26 @@ package SistemasInteligentes;
 
 public class testAgenteA {
     public static void main(String [] args) {
+        long res = 0;
+
         System.out.println("---------------------------------------------------------------");
         System.out.println("Prueba AgenteA");
         System.out.println();
-        Malla campo = new Malla(43, 8, 5, 5);
+        Malla campo = new Malla(43, 8, 5, 10);
         campo.ver();
 
         EstadoMalla estadoInicial = new EstadoMalla(campo, campo.getPosI());
         EstadoMalla estadoFinal = new EstadoMalla(campo, campo.getPosF());
 
+        long start = System.currentTimeMillis();
+
         AgenteA agenteA = new AgenteA();
         for(Estado x : agenteA.resuelve(estadoInicial, estadoFinal)){
             x.ver();
         }
-        System.out.print("time in seconds = ");
-        System.out.println(System.nanoTime() * 0.000000001);
+
+        long end = System.currentTimeMillis();
+        res = end - start;
+        System.out.println(res);
     }
 }
